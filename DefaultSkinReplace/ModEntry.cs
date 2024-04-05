97cea8d2f0237ee44c37d82ff17643be9e5ea04e
BussinBungus <146482804+BussinBungus@users.noreply.github.com>
2024-04-05 14:15:31 -0400

    Update ModEntry.cs

--- DefaultSkinReplace/ModEntry.cs
+++ DefaultSkinReplace/ModEntry.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using StardewValley.Characters;
 using StardewValley.GameData.FarmAnimals;
 
 namespace DefaultSkinReplace
@@ -20,9 +19,13 @@ namespace DefaultSkinReplace
         public override void Entry(IModHelper helper)
         {
             helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+
+            helper.ConsoleCommands.Add("dasr_reroll", "Rerolls skins for default animals. To make animals default, run 'dasr_reset all' first!\n\nUsage: dasr_reroll <name>\n- name: name of animal to reroll, or 'all' to reroll all.", this.Reroll);
+            helper.ConsoleCommands.Add("dasr_pick", "Picks a specific farm animal skin.\n\nUsage: dasr_pick <name> <skin>\n- name: name of animal to pick skin for.\n- skin: the skin to apply.", this.Pick);
+            helper.ConsoleCommands.Add("dasr_reset", "Resets to default skin.\n\nUsage: dasr_reset <name>\n- name: name of animal to reset, or 'all' to reset all.", this.Reset);
         }
 
-        /// <summary>Get all animals from the world and friendship data.</summary>
+        /// <summary>Get all farm animals from the world and friendship data.</summary>
         public IEnumerable<FarmAnimal> GetFarmAnimals()
         {
             List<FarmAnimal> animals = new List<FarmAnimal>();
@@ -44,34 +47,101 @@ namespace DefaultSkinReplace
         {
             foreach (FarmAnimal animal in this.GetFarmAnimals())
             {
-                FarmAnimalData data = animal.GetAnimalData();
+                RollSkin(animal);
+            }
+        }
+        private void RollSkin(FarmAnimal animal)
+        {
+            FarmAnimalData data = animal.GetAnimalData();
 
-                if (data != null)
+            if (data != null)
+            {
+                if (animal.skinID.Value == null && data.Skins != null)
                 {
-                    if (animal.skinID.Value == null && data.Skins != null)
+                    Monitor.Log($"Detected a skinless {animal.type}.");
+                    Random random = Utility.CreateRandom(animal.myID.Value);
+                    Monitor.Log($"Generated random from animal ID {animal.myID.Value}.");
+                    float totalWeight = 1f;
+                    foreach (FarmAnimalSkin skin2 in data.Skins)
                     {
-                        Monitor.Log($"Detected a skinless {animal.type}.");
-                        Random random = Utility.CreateRandom(animal.myID.Value);
-                        Monitor.Log($"Generated random from animal ID {animal.myID.Value}.");
-                        float totalWeight = 1f;
-                        foreach (FarmAnimalSkin skin2 in data.Skins)
+                        Monitor.Log($"Adding {skin2.Id} weight of {skin2.Weight}");
+                        totalWeight += skin2.Weight;
+                        Monitor.Log($"Total weight now {totalWeight}");
+                    }
+                    totalWeight = Utility.RandomFloat(0f, totalWeight, random);
+                    Monitor.Log($"Randomized, total weight now {totalWeight}");
+                    foreach (FarmAnimalSkin skin in data.Skins)
+                    {
+                        totalWeight -= skin.Weight;
+                        Monitor.Log($"{skin.Id} weight subtracted, total weight now {totalWeight}");
+                        if (totalWeight <= 0f)
                         {
-                            Monitor.Log($"Adding {skin2.Id} weight of {skin2.Weight}");
-                            totalWeight += skin2.Weight;
-                            Monitor.Log($"Total weight now {totalWeight}");
+                            animal.skinID.Value = skin.Id;
+                            Monitor.Log($"Total weight <0, skin set to {skin.Id}");
+                            break;
                         }
-                        totalWeight = Utility.RandomFloat(0f, totalWeight, random);
-                        Monitor.Log($"Randomized, total weight now {totalWeight}");
+                    }
+                }
+            }
+        }
+
+        private void Reroll(string command, string[] args)
+        {
+            foreach (FarmAnimal animal in this.GetFarmAnimals())
+            {
+                if (args[0] == "all" || args[0] == animal.Name)
+                {
+                    Monitor.Log($"Rerolling skin for {animal.type} {animal.Name}.", LogLevel.Info);
+                    RollSkin(animal);
+                }
+            }
+        }
+        private void Pick(string command, string[] args)
+        {
+            foreach (FarmAnimal animal in this.GetFarmAnimals())
+            {
+                string skinID = null;
+                
+                if (args[0] == animal.Name)
+                {
+                    FarmAnimalData data = animal.GetAnimalData();
+                    if (data != null && data.Skins != null)
+                    {
                         foreach (FarmAnimalSkin skin in data.Skins)
                         {
-                            totalWeight -= skin.Weight;
-                            Monitor.Log($"{skin.Id} weight subtracted, total weight now {totalWeight}");
-                            if (totalWeight <= 0f)
+                            if (skin.Id.Contains(args[1]))
                             {
-                                animal.skinID.Value = skin.Id;
-                                Monitor.Log($"Total weight <0, skin set to {skin.Id}");
+                                skinID = skin.Id;
                                 break;
                             }
+                        }
+
+                    if (skinID != null)
+                    {
+                        animal.skinID.Value = skinID;
+                        Monitor.Log($"{animal.type} {animal.Name} given skin {skinID}.", LogLevel.Info);
+                        break;
+                    }
+                    Monitor.Log($"Skin name may be incorrect.", LogLevel.Info);
+                    break;
+                    }
+                }  
+            }
+        }
+        private void Reset(string command, string[] args)
+        {
+            foreach (FarmAnimal animal in this.GetFarmAnimals())
+            {
+                if (args[0] == "all" || args[0] == animal.Name)
+                {
+                    FarmAnimalData data = animal.GetAnimalData();
+
+                    if (data != null)
+                    {
+                        if (animal.skinID.Value != null)
+                        {
+                            Monitor.Log($"Resetting {animal.type} {animal.Name} to default skin.", LogLevel.Info);
+                            animal.skinID.Value = null;
                         }
                     }
                 }
