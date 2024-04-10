d83d00223b3014afdc1678e47ce0ae9ed6c51047
BussinBungus <146482804+BussinBungus@users.noreply.github.com>
2024-04-09 23:00:29 -0400

    Replace DefaultBookTitles with CoverDictionary

---
+++ LostBookMenu/CoverDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LostBookMenu
+{
+    public partial class ModEntry
+    {
+        public Dictionary<string, CoverData> coverDictionary = new Dictionary<string, CoverData>()
+        {
+            //{ "Missing", new CoverData() },
+            { "0", new CoverData() },
+            { "1", new CoverData() },
+            { "2", new CoverData() },
+            { "3", new CoverData() },
+            { "4", new CoverData() },
+            { "5", new CoverData() },
+            { "6", new CoverData() },
+            { "7", new CoverData() },
+            { "8", new CoverData() },
+            { "9", new CoverData() },
+            { "10", new CoverData() },
+            { "11", new CoverData() },
+            { "12", new CoverData() },
+            { "13", new CoverData() },
+            { "14", new CoverData() },
+            { "15", new CoverData() },
+            { "16", new CoverData() },
+            { "17", new CoverData() },
+            { "18", new CoverData() },
+            { "19", new CoverData() },
+            { "20", new CoverData() },
+        };
+    }
+}
