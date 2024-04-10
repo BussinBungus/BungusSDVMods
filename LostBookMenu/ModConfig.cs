7750b8bcc818f0922a845dc13e81f975453cde86
BussinBungus <146482804+BussinBungus@users.noreply.github.com>
2024-04-09 23:01:25 -0400

    Update ModConfig.cs

--- LostBookMenu/ModConfig.cs
+++ LostBookMenu/ModConfig.cs
@@ -3,12 +3,26 @@ using StardewModdingAPI;
 namespace LostBookMenu
 {
     public class ModConfig
+    /// Original ///
+    /*{
+        public bool ModEnabled { get; set; } = true;
+        public bool MenuInLibrary { get; set; } = true;
+        public SButton MenuKey { get; set; } = SButton.None;
+        public bool LegacyTitles { get; set; } = true;
+        public int WindowWidth { get; set; } = 1600;
+        public int WindowHeight { get; set; } = 900;
+        public int GridColumns { get; set; } = 7;
+        public int xOffset { get; set; } = -1;
+        public int yOffset { get; set; } = 160;
+        public float CoverScale { get; set; } = 8;
+        public int HorizontalSpace { get; set; } = 96;
+        public int VerticalSpace { get; set; } = 116;
+    }*/
+    /// Smaller ///
     {
         public bool ModEnabled { get; set; } = true;
         public bool MenuInLibrary { get; set; } = true;
         public SButton MenuKey { get; set; } = SButton.None;
-        public string MenuTitle { get; set; } = "Stardew Valley Library";
-        public string MissingText { get; set; } = "???";
         public bool LegacyTitles { get; set; } = false;
         public int WindowWidth { get; set; } = 1050;
         public int WindowHeight { get; set; } = 600;
